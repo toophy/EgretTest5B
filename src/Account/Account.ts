8b207c64feb4f938b958104b78411faa7127a619
toophy@vip.qq.com <toophy@vip.qq.com>
2017-10-09 21:26:42 +0800

    大账号环境

--- src/Account/Account.ts
+++ src/Account/Account.ts
@@ -3,6 +3,9 @@
 namespace tgame {
     // AccountEnv 帐号环境
     export class AccountEnv {
+        // 上层显示对象
+        private rootDisplay: eui.UILayer;
+
         // 基本配置
         public name: string; // 名称
         public pwd: string; // 密码
@@ -13,17 +16,27 @@ namespace tgame {
         public sceneConn: Network;// 场景网络连接
         public chatConn: Network; //聊天网络连接
 
-        // 窗口
-        public loginDlg: LoginDlg;// 登录窗口
+        // 龙骨动画
+        public factory: dragonBones.EgretFactory = new dragonBones.EgretFactory();
 
-        // 游戏场景
-        public gameScene: GameMapContainer; // 主场景
+        // 窗口
+        public loginDlg: tui.LoginDlg;// 登录窗口
+
+        // 大陆场景
+        private _lands: tgame.LandView;
+        //
+        public static G: number = 0.6;
+
 
         // 构造函数
-        constructor(name: string, pwd: string) {
-            this.name = name;
-            this.pwd = pwd;
+        constructor(rootDisplay: eui.UILayer) {
+            this.rootDisplay = rootDisplay;
             this.state = "初始化";
+        }
+
+        // 获取主显示对象
+        public GetRootDisplay(): eui.UILayer {
+            return this.rootDisplay;
         }
 
         // 状态机
@@ -39,13 +52,99 @@ namespace tgame {
         public ChangeState(state: string, data: any): void {
             switch (state) {
                 case "帐号登录":
-                    // 当前是 "初始化", "游戏场景登录", ... 做不同的处理
+                    if (this.doAccountLogin(state, data)) {
+                        this.state = state;
+                        this.stateData = data;
+                    }
                     break;
                 case "游戏场景登录":
                     break;
             }
-            this.state = state;
-            this.stateData = data;
+        }
+
+        private doAccountLogin(state: string, data: any): boolean {
+            // 当前呢
+            let ret: boolean = false;
+            switch (this.state) {
+                case "初始化":
+                    {
+                        this.name = data["name"];
+                        this.pwd = data["pwd"]
+                        // 打开登录连接 发送登录消息
+                        //
+                        ret = true;
+                    }
+                    break;
+                case "游戏场景登录":
+                    break;
+            }
+
+            return false;
+        }
+
+        // 加载龙骨动画到工厂
+        public loadDragon(conf_json: string, texture_json: string, textur_img: string) {
+            // "CoreElement_json"
+            // "CoreElement_texture_1_json"
+            // "CoreElement_texture_1_png"
+            this.factory.parseDragonBonesData(RES.getRes(conf_json));
+            this.factory.parseTextureAtlasData(RES.getRes(texture_json), RES.getRes(textur_img));
+        }
+
+        public OnTouchMove(x: number, y: number) {
+        }
+
+        public OnTouchHandler(event: egret.TouchEvent): void {
+        }
+
+        public OnKeyHandler(event: KeyboardEvent): void {
+        }
+
+        public OnUpdateFrame(evt: egret.Event) {
+            dragonBones.WorldClock.clock.advanceTime(-1);
+        }
+
+        /**
+         * 临时账号环境初始化
+         */
+        public OnInited() {
+            // 显示登录窗口
+            if (this.loginDlg == null) {
+                this.loginDlg = new tui.LoginDlg();
+                this.rootDisplay.addChild(this.loginDlg);
+            }
+        }
+
+        /**
+         * 正式账号创建完成
+         */
+        public OnCreated(account: string, pwd: string) {
+
+            // let data = RES.getRes("land_json");
+            // this._lands = new tgame.LandView();
+            // this._lands.LoadLand(data);
+            // this._lands.ShowLand(this);
+        }
+
+        /**
+         * 本账号为激活状态
+         */
+        public OnActive() {
+
+        }
+
+        /**
+         * 本账户为后台状态
+         */
+        public OnDisActive() {
+
+        }
+
+        /**
+         * 本账号被摧毁
+         */
+        public OnDestroy() {
+
         }
     }
 }
--- src/MyEUI/LoingDlg.ts
+++ src/MyEUI/LoingDlg.ts
@@ -1,58 +1,38 @@
+namespace tui {
+	export class LoginDlg extends eui.Component {
 
-class LoginDlg extends eui.Component {
+		private accountEnv: tgame.AccountEnv;
 
-	private static _instance: LoginDlg;
+		private Lgn_AccountID: eui.TextInput;
+		private Lgn_AccountPwdID: eui.TextInput;
+		private Lgn_EnterID: eui.Button;
+		private Lgn_LeaveID: eui.Button;
 
-	public static getInstance(): LoginDlg {
-		if (!LoginDlg._instance) {
-			LoginDlg._instance = new LoginDlg();
+		constructor() {
+			super();
+			this.skinName = "resource/assets/MainUI/LoginDlg/LoginDlg.exml";
+			this.addEventListener(eui.UIEvent.COMPLETE, this.uiCompHandler, this);
 		}
-		return LoginDlg._instance
+
+		protected childrenCreated(): void {
+			super.childrenCreated();
+		}
+
+		private uiCompHandler(): void {
+
+			this.x = this.stage.stageWidth / 2 - this.skin.width / 2;
+			this.y = this.stage.stageHeight / 2 - this.skin.height / 2;
+
+			// 注册事件 : 点击登录按钮
+			this.Lgn_EnterID.addEventListener(egret.TouchEvent.TOUCH_TAP, (event: egret.TouchEvent) => {
+				this.visible = false;
+			}, this);
+
+			// 注册事件 : 点击离开按钮
+			this.Lgn_LeaveID.addEventListener(egret.TouchEvent.TOUCH_TAP, (event: egret.TouchEvent) => {
+				console.log("点击离开按钮");
+				// this.visible = false;
+			}, this);
+		}
 	}
-
-	private Lgn_AccountID: eui.TextInput;
-	private Lgn_AccountPwdID: eui.TextInput;
-	private Lgn_EnterID: eui.Button;
-	private Lgn_LeaveID: eui.Button;
-
-
-    private gameMapContainer: tgame.GameMapContainer;
-
-	constructor() {
-		super();
-		this.skinName = "resource/assets/MainUI/LoginDlg/LoginDlg.exml";
-		this.addEventListener(eui.UIEvent.COMPLETE, this.uiCompHandler, this);
-	}
-
-	protected childrenCreated(): void {
-		super.childrenCreated();
-	}
-
-	private uiCompHandler(): void {
-
-		this.x = this.stage.stageWidth / 2 - this.skin.width / 2;
-        this.y = this.stage.stageHeight / 2 - this.skin.height / 2;
-
-		// 注册事件 : 点击登录按钮
-		this.Lgn_EnterID.addEventListener(egret.TouchEvent.TOUCH_TAP, (event: egret.TouchEvent) => {
-			this.startCreateSceneNew()
-			this.visible = false;
-		}, this);
-
-		// 注册事件 : 点击离开按钮
-		this.Lgn_LeaveID.addEventListener(egret.TouchEvent.TOUCH_TAP, (event: egret.TouchEvent) => {
-			console.log("点击离开按钮");
-			// this.visible = false;
-		}, this);
-	}
-
-	/**
-     *  创建场景界面
-     * Create scene interface
-     */
-    protected startCreateSceneNew(): void {
-        this.gameMapContainer = new tgame.GameMapContainer(this.parent);
-        this.gameMapContainer.createScene();
-        this.parent.addChild(this.gameMapContainer);
-    }
 }
